4ae272a5296359694d6f09a868aae26733b5bd14
hristo <hristo.m.stefanov@gmail.com>
2020-04-29 17:14:02 +0300

    close the app when navigating back from login

--- app/src/main/java/hristostefanov/starlingdemo/ui/AccessTokenFragment.kt
+++ app/src/main/java/hristostefanov/starlingdemo/ui/AccessTokenFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.AbstractSavedStateViewModelFactory
@@ -36,6 +37,14 @@ class AccessTokenFragment : Fragment() {
         binding.viewmodel = viewModel
 
         accessTokenEditText.requestFocus()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            requireActivity().finish()
+        }
     }
 
     private inner class ViewModelFactory :
