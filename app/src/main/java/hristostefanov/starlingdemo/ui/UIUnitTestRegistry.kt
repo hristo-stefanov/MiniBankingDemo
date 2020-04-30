14df9161e8f42044dc3233b4baf4afe2b23ada69
hristo <hristo.m.stefanov@gmail.com>
2020-04-30 17:56:30 +0300

    add comment

--- app/src/main/java/hristostefanov/starlingdemo/ui/UIUnitTestRegistry.kt
+++ app/src/main/java/hristostefanov/starlingdemo/ui/UIUnitTestRegistry.kt
@@ -3,5 +3,12 @@ package hristostefanov.starlingdemo.ui
 import androidx.lifecycle.ViewModelProvider
 
 object UIUnitTestRegistry {
+    // Replacing the ViewModelProvider.Factory instead of ViewModel itself in Fragments
+    // being UI unit tested is the working approach. Otherwise, during instrumented testing
+    // AbstractSavedStateViewModelFactory#create in the Fragment would try to call
+    // ViewModel#setTagIfAbsent which will crash because a field being referenced is null
+    // due to mocking. Probably this will work if dexmaker-mockito-inline used.
+    // The root issue here is that mocking a view model involves mocking a type that we do not own -
+    // the base class ViewModel.
     var viewModelFactory: ViewModelProvider.Factory? = null
 }
