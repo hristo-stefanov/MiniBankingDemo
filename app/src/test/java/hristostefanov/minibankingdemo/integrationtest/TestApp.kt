ab0be973b37f0606a95556c0aa4dc72b18d0baaa
hristo <hristo.m.stefanov@gamil.com>
2021-09-26 11:14:20 +0100

    Add TODO

--- app/src/test/java/hristostefanov/minibankingdemo/integrationtest/TestApp.kt
+++ app/src/test/java/hristostefanov/minibankingdemo/integrationtest/TestApp.kt
@@ -2,7 +2,7 @@ package hristostefanov.minibankingdemo.integrationtest
 
 import hristostefanov.minibankingdemo.integrationtest.di.DaggerTestApplicationComponent
 import hristostefanov.minibankingdemo.integrationtest.di.TestApplicationComponent
-
+// TODO see https://developer.android.com/training/dependency-injection/dagger-android#dagger-end-to-end-tests
 object TestApp {
     private var _component: TestApplicationComponent = DaggerTestApplicationComponent.create()
     val component = _component
