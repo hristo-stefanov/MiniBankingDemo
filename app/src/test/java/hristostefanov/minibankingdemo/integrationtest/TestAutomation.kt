464de5f2e5a60b322482e9009f2d4069c08fc50d
hristo <hristo.m.stefanov@gamil.com>
2021-09-26 11:12:17 +0100

    Add login() to TestAutomation

--- app/src/test/java/hristostefanov/minibankingdemo/integrationtest/TestAutomation.kt
+++ app/src/test/java/hristostefanov/minibankingdemo/integrationtest/TestAutomation.kt
@@ -4,6 +4,7 @@ import hristostefanov.minibankingdemo.presentation.AccountsViewModel
 import java.math.BigDecimal
 
 interface TestAutomation {
+    fun login()
     fun createAccount(number: String, currency: String, transactions: List<BigDecimal>)
     fun calculateRoundUp(accountNumber: String): BigDecimal
     fun theCalculatedRoundUpIsOne()
