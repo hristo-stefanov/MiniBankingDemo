1f4f2855af9644639b33e664a0bfd91d82aed295
hristo <hristo.m.stefanov@gamil.com>
2021-10-08 18:12:23 +0100

    Remove TODO

--- app/src/test/java/hristostefanov/minibankingdemo/acceptancetest/technical/TestApp.kt
+++ app/src/test/java/hristostefanov/minibankingdemo/acceptancetest/technical/TestApp.kt
@@ -2,7 +2,6 @@ package hristostefanov.minibankingdemo.acceptancetest.technical
 
 import hristostefanov.minibankingdemo.acceptancetest.technical.di.DaggerTestApplicationComponent
 import hristostefanov.minibankingdemo.acceptancetest.technical.di.TestApplicationComponent
-// TODO see https://developer.android.com/training/dependency-injection/dagger-android#dagger-end-to-end-tests
 object TestApp {
     private var _component: TestApplicationComponent = DaggerTestApplicationComponent.create()
     val component = _component
