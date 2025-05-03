3aca585ef170d2cfe0efe51d0de9bdf3bd486b23
hristo <hristo.m.stefanov@gamil.com>
2025-05-03 16:24:45 +0100

    Optmize round-up calculation

--- app/src/main/java/hristostefanov/minibankingdemo/business/Calculations.kt
+++ app/src/main/java/hristostefanov/minibankingdemo/business/Calculations.kt
@@ -4,21 +4,16 @@ import hristostefanov.minibankingdemo.business.entities.Source
 import hristostefanov.minibankingdemo.business.entities.Status
 import hristostefanov.minibankingdemo.business.entities.Transaction
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 fun isTransactionEligibleForRoundup(transaction: Transaction) =
     transaction.amount.signum() == -1
             && transaction.status == Status.SETTLED
             && transaction.source == Source.EXTERNAL
 
+/**
+ * Implements this FEEL expression: sum(for t in transactions return ceiling(t) - t)
+ */
 fun calcRoundup(amounts: List<BigDecimal>): BigDecimal = amounts
-    // TODO optimize processing
-    // get the fractional part
-    .map { it.remainder(BigDecimal.ONE) }
-    // consider only greater than zero fractional parts (zero's complement to 1 is 1)
-    .filter { it.signum() == 1 }
-    // get the complement to 1
-    .map { BigDecimal.ONE.minus(it) }
-    // accumulate the complements
-    // NOTE unlike #reduce, #fold allows empty collection by getting the initial value
-    // as argument instead of using the first element of the collection
-    .fold(BigDecimal.ZERO) { acc, item -> acc.add(item) }
+    .map { it.setScale(0, RoundingMode.CEILING).minus(it) }
+    .fold(BigDecimal.ZERO, BigDecimal::add)
