4ebfa0c1aa133157b9ce432cd0125da0ee3e397b
Hristo "Chris" Stefanov <hristo.m.stefanov@gmail.com>
2025-07-20 12:55:16 +0100

    Add debug loggin and resuming interactors in MainViewModel

--- app/src/main/java/hristostefanov/minibankingdemo/presentation/MainViewModel.kt
+++ app/src/main/java/hristostefanov/minibankingdemo/presentation/MainViewModel.kt
@@ -1,5 +1,6 @@
 package hristostefanov.minibankingdemo.presentation
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,37 +14,62 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 private const val IS_STARTUP_INTERACTOR_ACTIVE_KEY = "isStartupInteractorActive"
+private const val IS_PRESENT_SUMMARY_INTERACTIVE_KEY = "isPresentSummaryInteractorActive"
+private val LOG_TAG = MainViewModel::class.simpleName
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     private val startupInteractor: StartupInteractor,
+    private val loginSessionRegistry: LoginSessionRegistry,
     private val userInterface: UserInterfaceImpl,
     private val sessionRegistry: LoginSessionRegistry
 ) : ViewModel() {
 
     init {
-        startStartupInteractorIfNotActive()
-    }
-
-    private fun startStartupInteractorIfNotActive() {
         val isStartupInteractorActive: Boolean? = savedStateHandle[IS_STARTUP_INTERACTOR_ACTIVE_KEY]
+        Log.d(LOG_TAG, "Init: isStartupInteractorActive = $isStartupInteractorActive")
 
-        startupInteractor.status
-            .onEach {
-                savedStateHandle[IS_STARTUP_INTERACTOR_ACTIVE_KEY] = it.isActive()
-            }
-            .launchIn(viewModelScope)
+        val isPresentSummaryInteractorActive: Boolean? = savedStateHandle[IS_PRESENT_SUMMARY_INTERACTIVE_KEY]
+        Log.d(LOG_TAG, "Init: isPresentSummaryInteractorActive - $isPresentSummaryInteractorActive")
 
-        if (isStartupInteractorActive != true) {
-            viewModelScope.launch {
+        // Should be exactly here - after getting the saved values and before
+        // starting interactors.
+        keepSavingFlagsForActiveInteractors()
+
+        viewModelScope.launch {
+            if (isStartupInteractorActive == true) {
+                startupInteractor.resume()
+            } else {
                 startupInteractor.start(userInterface)
+            }
+
+            if (isPresentSummaryInteractorActive == true) {
+                loginSessionRegistry.component?.presentAccountsAndRoundupsSummary?.resume()
             }
         }
     }
 
+    private fun keepSavingFlagsForActiveInteractors() {
+        startupInteractor.status
+            .onEach {
+                Log.d(LOG_TAG, "StartupInteractor.status = $it")
+                savedStateHandle[IS_STARTUP_INTERACTOR_ACTIVE_KEY] = it.isActive()
+            }
+            .launchIn(viewModelScope)
+
+        loginSessionRegistry.component?.presentAccountsAndRoundupsSummary?.status
+            ?.onEach {
+                Log.d(LOG_TAG, "PresentSummaryInteractor.status = $it")
+                savedStateHandle[IS_PRESENT_SUMMARY_INTERACTIVE_KEY] = it.isActive()
+            }
+            ?.launchIn(viewModelScope)
+    }
+
     internal fun executeContinuation(continuationId: String, param: String? = null) =
         viewModelScope.launch {
+            Log.d(LOG_TAG, "executeContinuation: continuationId = $continuationId param = $param")
+
             when (ContinuationId.valueOf(continuationId)) {
                 ContinuationId.Startup_LoginCredentialsSubmit -> startupInteractor.onLoginCredentialsSubmit(
                     param!!,
