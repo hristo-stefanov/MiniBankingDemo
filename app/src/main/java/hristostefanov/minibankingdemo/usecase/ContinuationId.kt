37a23b7f59bd4f17b46b290c29d8dd15a29fb34d
Hristo "Chris" Stefanov <hristo.m.stefanov@gmail.com>
2025-09-21 18:48:49 +0100

    Optimize the delegation from GetSummaryInteractor and EnsureLoginCredentialsInteractor

--- app/src/main/java/hristostefanov/minibankingdemo/usecase/ContinuationId.kt
+++ app/src/main/java/hristostefanov/minibankingdemo/usecase/ContinuationId.kt
@@ -2,7 +2,6 @@ package hristostefanov.minibankingdemo.usecase
 
 enum class ContinuationId {
     GetSummary_RetryLoading,
-    GetSummary_LoginCredentialsEnsured,
     TransferRoundUp_SavingsGoalSelected,
     TransferRoundUp_Confirmed,
     CreateSavingsGoal_NameSubmitted
